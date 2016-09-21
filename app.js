51790dca62ca6c4a18e47cd8fb70cbaaa8322bd5
Meagan Mason <meaganmason83@gmail.com>
2016-09-20 22:22:16 -0700

    Corrected formatting erros in app.js

--- app.js
+++ app.js
@@ -4,112 +4,116 @@ var userName = prompt('What\'s your name?');
 alert('Welcome to my site, ' + userName + '!');
 
 
-var meaganFunction = function () {
-var questionOne = prompt("Do you think I'm from the United States?").toLowerCase();
+var meaganFunction = function() {
 
-if (questionOne === 'yes' || questionOne === 'y') {
-  alert ("Yup, you got it!");
-} else if (questionOne === 'no' || questionOne === 'n') {
-  alert ("You think I'm exotic then, how sweet!");
-} else {
-  alert ("Please answer yes or no.");
-}
+  var questionOne = prompt("Do you think I'm from the United States?").toLowerCase();
+
+  if (questionOne === 'yes' || questionOne === 'y') {
+    alert ("Yup, you got it!");
+  } else if (questionOne === 'no' || questionOne === 'n') {
+    alert ("You think I'm exotic then, how sweet!");
+  } else {
+    alert ("Please answer yes or no.");
+  }
 };
 meaganFunction();
 
-var meagantwoFunction = function () {
-var questionTwo = prompt("Do you think I'm a big geek?").toLowerCase();
+var meagantwoFunction = function() {
+  var questionTwo = prompt("Do you think I'm a big geek?").toLowerCase();
 
-if (questionTwo === 'yes' || questionTwo === 'y'){
-  alert ("How dare you.");
-} else if (questionTwo === 'no' || questionTwo === 'n'){
-  alert ("You're right, I'm only half-geek.");
-} else {
-  alert ("Please answer yes or no.");
-}
+  if (questionTwo === 'yes' || questionTwo === 'y') {
+    alert ("How dare you.");
+  } else if (questionTwo === 'no' || questionTwo === 'n') {
+    alert ("You're right, I'm only half-geek.");
+  } else {
+    alert ("Please answer yes or no.");
+  }
 };
 meagantwoFunction();
 
-var meaganthreeFunction = function () {
-var questionThree = prompt("Do you think I still get carded?").toLowerCase();
+var meaganthreeFunction = function() {
+  var questionThree = prompt("Do you think I still get carded?").toLowerCase();
 
-if (questionThree === 'yes' || questionThree === 'y') {
-  alert ("You're right, and I take it as a compliment. Besides, 33 is the new 23!");
-} else if (questionThree === 'no' || questionThree === 'n'){
-  alert ("How dare you.");
-} else {
-  alert ("Please answer yes or no.");
-}
+  if (questionThree === 'yes' || questionThree === 'y') {
+    alert ("You're right, and I take it as a compliment. Besides, 33 is the new 23!");
+  } else if (questionThree === 'no' || questionThree === 'n') {
+    alert ("How dare you.");
+  } else {
+    alert ("Please answer yes or no.");
+  }
 };
-meaganthreeFunction ();
+meaganthreeFunction();
 
-var meaganfourFunction = function () {
-var questionFour = prompt("Do you think I have road rage?").toLowerCase();
+var meaganfourFunction = function() {
+  var questionFour = prompt("Do you think I have road rage?").toLowerCase();
 
-if (questionFour === 'yes' || questionFour === 'y'){
-  alert ("Guitly as charged.");
-} else if (questionFour === 'no' || questionFour === 'n'){
-  alert ("Ohhh, you have no idea! LOL");
-} else {
-  alert ("Please answer yes or no.");
-}
+  if (questionFour === 'yes' || questionFour === 'y') {
+    alert ("Guitly as charged.");
+  } else if (questionFour === 'no' || questionFour === 'n') {
+    alert ("Ohhh, you have no idea! LOL");
+  } else {
+    alert ("Please answer yes or no.");
+  }
 };
 meaganfourFunction();
 
-var meaganfiveFunction = function () {
-var questionFive = prompt("Would you say I'm at least 5 feet tall?").toLowerCase();
+var meaganfiveFunction = function() {
+  var questionFive = prompt("Would you say I'm at least 5 feet tall?").toLowerCase();
 
-if (questionFive === 'yes' || questionFive === 'y'){
-  alert ("Good, you don't think I'm a midget.");
-} else if (questionFive === 'no' || questionFive === 'n'){
-  alert ("You really think I'm a midget?");
-} else {
-  alert ("Please answer yes or no.");
-}
+  if (questionFive === 'yes' || questionFive === 'y') {
+    alert ("Good, you don't think I'm a midget.");
+  } else if (questionFive === 'no' || questionFive === 'n') {
+    alert ("You really think I'm a midget?");
+  } else {
+    alert ("Please answer yes or no.");
+  }
 };
 meaganfiveFunction();
 
-var meagansixFunction = function () {
-var questionSix = parseInt(prompt("I'm thinking of a number between 1 and 5. What is it?"));
+var meagansixFunction = function() {
+  var questionSix = parseInt(prompt("I'm thinking of a number between 1 and 5. What is it?"));
 
-var myArray = [3, 4];
-if (myArray.indexOf(questionSix) > -1) {
-  alert ("You can read my mind!");
-} else {
-  alert ("You aren't a mind reader afterall.");
-}
+  var myArray = [3, 4];
+
+  if (myArray.indexOf(questionSix) > -1) {
+    alert ("You can read my mind!");
+  } else {
+    alert ("You aren't a mind reader afterall.");
+  }
 };
 meagansixFunction();
 
-var meagansevenFunction = function () {
+var meagansevenFunction = function() {
 
-var myAnswer = 8;
-var guesses = 3
-for (var i = 0; i < guesses; ++i) {
-  var questionSeven = parseInt(prompt("Now I'm thinking of a different number between 5-10. What is it?"))
-  if (questionSeven === myAnswer) {
-  alert ("Correct.");
-  break;
-  } else if (questionSeven > myAnswer) {
-  alert ("Too high.");
-  } else if (questionSeven < myAnswer) {
-  alert ("Too low.");
-  } else {
-  alert("Please enter a number.");
-  }
+  var myAnswer = 8;
+  var guesses = 3
+
+  for (var i = 0; i < guesses; ++i) {
+    var questionSeven = parseInt(prompt("Now I'm thinking of a different number between 5-10. What is it?"))
+
+    if (questionSeven === myAnswer) {
+      alert ("Correct.");
+      break;
+    } else if (questionSeven > myAnswer) {
+      alert ("Too high.");
+    } else if (questionSeven < myAnswer) {
+      alert ("Too low.");
+    } else {
+      alert("Please enter a number.");
+    }
   }
 };
 meagansevenFunction();
 
 var meaganeightFunction = function() {
-var questionEight = prompt("Which Seattle neighborhoods have I lived in?").toLowerCase();
+  var questionEight = prompt("Which Seattle neighborhoods have I lived in?").toLowerCase();
 
-var myHood = ['University District', 'Wallingford', 'Phinney Ridge', 'West Seattle', 'Capitol Hill'];
+  var myHood = ['University District', 'Wallingford', 'Phinney Ridge', 'West Seattle', 'Capitol Hill'];
 
-if (myHood.indexOf(questionEight) > -1) {
-  alert("Bingo!");
-} else if (myHood.indexOf(questionEight) != myHood) {
-	alert("Keep guessing.")
-}
+  if (myHood.indexOf(questionEight) > -1) {
+    alert("Bingo!");
+  } else if (myHood.indexOf(questionEight) != myHood) {
+    alert("Keep guessing.")
+  }
 };
 meaganeightFunction();
