dbe50bc0ad945227699891a88fc91961d85b821d
Meagan Mason <meaganmason83@gmail.com>
2016-09-22 18:29:01 -0700

    corrected my question eight per Dan's feedback.

--- app.js
+++ app.js
@@ -6,79 +6,79 @@ alert('Welcome to my site, ' + userName + '!');
 
 var meaganFunction = function() {
 
-  var questionOne = prompt("Do you think I'm from the United States?").toLowerCase();
+  var questionOne = prompt('Do you think I\'m from the United States?').toLowerCase();
 
   if (questionOne === 'yes' || questionOne === 'y') {
-    alert ("Yup, you got it!");
+    alert ('Yup, you got it!');
   } else if (questionOne === 'no' || questionOne === 'n') {
-    alert ("You think I'm exotic then, how sweet!");
+    alert ('You think I\'m exotic then, how sweet!');
   } else {
-    alert ("Please answer yes or no.");
+    alert ('Please answer yes or no.');
   }
 };
 meaganFunction();
 
 var meagantwoFunction = function() {
-  var questionTwo = prompt("Do you think I'm a big geek?").toLowerCase();
+  var questionTwo = prompt('Do you think I\'m a big geek?').toLowerCase();
 
   if (questionTwo === 'yes' || questionTwo === 'y') {
-    alert ("How dare you.");
+    alert ('How dare you.');
   } else if (questionTwo === 'no' || questionTwo === 'n') {
-    alert ("You're right, I'm only half-geek.");
+    alert ('You\'re right, I\'m only half-geek.');
   } else {
-    alert ("Please answer yes or no.");
+    alert ('Please answer yes or no.');
   }
 };
 meagantwoFunction();
 
 var meaganthreeFunction = function() {
-  var questionThree = prompt("Do you think I still get carded?").toLowerCase();
+  var questionThree = prompt('Do you think I still get carded?').toLowerCase();
 
   if (questionThree === 'yes' || questionThree === 'y') {
-    alert ("You're right, and I take it as a compliment. Besides, 33 is the new 23!");
+    alert ('You\'re right, and I take it as a compliment. Besides, 33 is the new 23!');
   } else if (questionThree === 'no' || questionThree === 'n') {
-    alert ("How dare you.");
+    alert ('How dare you.');
   } else {
-    alert ("Please answer yes or no.");
+    alert ('Please answer yes or no.');
   }
 };
 meaganthreeFunction();
 
 var meaganfourFunction = function() {
-  var questionFour = prompt("Do you think I have road rage?").toLowerCase();
+  var questionFour = prompt('Do you think I have road rage?').toLowerCase();
 
   if (questionFour === 'yes' || questionFour === 'y') {
-    alert ("Guitly as charged.");
+    alert ('Guitly as charged.');
   } else if (questionFour === 'no' || questionFour === 'n') {
-    alert ("Ohhh, you have no idea! LOL");
+    alert ('Ohhh, you have no idea! LOL');
   } else {
-    alert ("Please answer yes or no.");
+    alert ('Please answer yes or no.');
   }
 };
 meaganfourFunction();
 
 var meaganfiveFunction = function() {
-  var questionFive = prompt("Would you say I'm at least 5 feet tall?").toLowerCase();
+  var questionFive = prompt('Would you say I\'m at least 5 feet tall?').toLowerCase();
 
   if (questionFive === 'yes' || questionFive === 'y') {
-    alert ("Good, you don't think I'm a midget.");
+    alert ('Good, you don\'t think I\'m a midget.');
   } else if (questionFive === 'no' || questionFive === 'n') {
-    alert ("You really think I'm a midget?");
+    alert ('You really think I\'m a midget?');
   } else {
-    alert ("Please answer yes or no.");
+    alert ('Please answer yes or no.');
   }
 };
 meaganfiveFunction();
 
 var meagansixFunction = function() {
-  var questionSix = parseInt(prompt("I'm thinking of a number between 1 and 5. What is it?"));
+  var questionSix = parseInt(prompt('I\'m thinking of a number between 1 and 5. What is it?'));
 
   var myArray = [3, 4];
 
   if (myArray.indexOf(questionSix) > -1) {
-    alert ("You can read my mind!");
+    alert ('You can read my mind!');
   } else {
-    alert ("You aren't a mind reader afterall.");
+    alert ('You aren\'t a mind reader afterall.');
   }
 };
 meagansixFunction();
@@ -86,34 +86,34 @@ meagansixFunction();
 var meagansevenFunction = function() {
 
   var myAnswer = 8;
-  var guesses = 3
+  var guesses = 3;
 
   for (var i = 0; i < guesses; ++i) {
-    var questionSeven = parseInt(prompt("Now I'm thinking of a different number between 5-10. What is it?"))
+    var questionSeven = parseInt(prompt('Now I\'m thinking of a number between 5 and 10. What is it?'));
 
     if (questionSeven === myAnswer) {
-      alert ("Correct.");
+      alert ('Correct.');
       break;
     } else if (questionSeven > myAnswer) {
-      alert ("Too high.");
+      alert ('Too high.');
     } else if (questionSeven < myAnswer) {
-      alert ("Too low.");
+      alert ('Too low.');
     } else {
-      alert("Please enter a number.");
+      alert('Please enter a number.');
     }
   }
 };
 meagansevenFunction();
 
 var meaganeightFunction = function() {
-  var questionEight = prompt("Which Seattle neighborhoods have I lived in?").toLowerCase();
+  var questionEight = prompt('Which Seattle neighborhoods have I lived in?').toLowerCase();
 
-  var myHood = ['University District', 'Wallingford', 'Phinney Ridge', 'West Seattle', 'Capitol Hill'];
+  var myHood = ['university district', 'wallingford', 'phinney ridge', 'west seattle', 'capitol hill'];
 
   if (myHood.indexOf(questionEight) > -1) {
-    alert("Bingo!");
-  } else if (myHood.indexOf(questionEight) != myHood) {
-    alert("Keep guessing.")
+    alert('Bingo!');
+  } else {
+    alert('Keep guessing.');
   }
 };
 meaganeightFunction();
